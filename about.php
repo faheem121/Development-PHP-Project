690ed876b49bb1a41d756118ad293eaa486b8ee2
Ahmad obaid <ahmadobaid169@gmail.com>
2020-01-23 14:14:11 +0500

    about us page is completed

---
+++ about.php
@@ -0,0 +1,283 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+   
+<!-- Mirrored from magicclubgroup.com/bapor/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 01 Jan 2020 19:53:07 GMT -->
+<head>
+      <meta charset="utf-8">
+      <title>Bapor - Responsive Ecommerce Html Template</title>
+      <meta name="description" content="Bapor Template" />
+      <meta name="keywords" content="bapor, WooCommerce, bootstrap, html template, bapor template">
+      <meta name="author" content="bapor" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <!--[if IE]>
+      <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+      <![endif]-->
+      <!-- Favicone Icon -->
+      <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+      <link rel="icon" type="img/png" href="img/favicon.png">
+      <link rel="apple-touch-icon" href="img/favicon.png">
+      <!-- start css -->
+      <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+      <!-- bootstrap css -->
+      <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+      <!-- fontawesome css -->
+      <link href="css/animate.min.css" rel="stylesheet" type="text/css" />
+      <!-- animate css -->
+      <link href="css/owl.carousel.css" rel="stylesheet" type="text/css" />
+      <!-- owl carousel css -->
+      <link href="css/slick.css" rel="stylesheet" type="text/css" />
+      <!-- slick css -->
+      <link href="css/style.css" rel="stylesheet" type="text/css" />
+      <!-- style css -->
+      <!-- end css -->
+   </head>
+   <body>
+     <?php include("header.php") ?>
+         <!-- Page Wraper -->
+         <div class="page-wraper">
+            <!--  Bread Crumb -->
+            <section class="breadcrumb">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <ul class="breadcrumb-link">
+                           <li><a href="index.php">Home</a></li>
+                           <li>About Us</li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </section>
+            <!-- Bread Crumb -->
+            <!-- Content page wrapper -->
+            <section class="content-page-wrapper pb-0">
+               <div class="page-title-wrapper">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-sm-12">
+                           <div class="section-heading text-center">
+                              <h2>About Us</h2>                           
+                           </div>                           
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="about-top-wrapper section-padding-bottom">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-md-6">
+                           <h3>We Are Best</h3>
+                           <p> Lorem Ipsum is simply dummy text ever sincehar the 1500s, when an unknownshil printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries. Simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                           <p> Lorem Ipsum is simply dummy text ever sincehar the 1500s, when an unknownshil printer took a galley of type and scrambled it to make a type specimen book. </p>
+                           <a href="#" class="btn btn-sm btn-color">Read More</a>
+                        </div>
+                        <div class="col-md-6 mt-sm-30">  
+                           <img src="img/bg/about_us.jpg" alt="" />                     
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="about-counter-wrapper parallex-image overlay-dark40 section-padding dark-bg" data-background-img="img/bg/bg_3.jpg" data-bg-position="center center">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-sm-3 mb-sm-30">
+                           <div class="counter-box">
+                              <div class="counter-icon">
+                                 <i class="fa fa-lightbulb-o" aria-hidden="true"></i>
+                              </div>
+                              <div class="counter-text">
+                                 <h2 class="counter" data-count="499">0</h2>
+                                 <h6>Completed Project</h6>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-sm-3 mb-sm-30">
+                           <div class="counter-box">
+                              <div class="counter-icon">
+                                 <i class="fa fa-heart-o" aria-hidden="true"></i>
+                              </div>
+                              <div class="counter-text">
+                                 <h2 class="counter" data-count="999">0</h2>
+                                 <h6>Happy Clients</h6>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-sm-3 mb-sm-30">
+                           <div class="counter-box">
+                              <div class="counter-icon">
+                                 <i class="fa fa-coffee" aria-hidden="true"></i>
+                              </div>
+                              <div class="counter-text">
+                                 <h2 class="counter" data-count="799">0</h2>
+                                 <h6>Team Member</h6>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-sm-3">
+                           <div class="counter-box">
+                              <div class="counter-icon">
+                                 <i class="fa fa-trophy" aria-hidden="true"></i>
+                              </div>
+                              <div class="counter-text">
+                                 <h2 class="counter" data-count="449">0</h2>
+                                 <h6>Best Awards</h6>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="our-team section-padding">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="section-heading text-center">
+                              <h2>Meet Our Team</h2> 
+                              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                           </div>                           
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-ms-6 col-xs-12 mb-sm-30">
+                           <div class="team-box text-center">
+                              <a class="team-img" href="#">
+                                 <img src="img/team/team01.png" alt="" />
+                              </a>
+                              <div class="team-text">
+                                 <h5><a href="#">John Doe</a></h5>
+                                 <p>CEO</p>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-ms-6 col-xs-12 mb-sm-30">
+                           <div class="team-box text-center">
+                              <a class="team-img" href="#">
+                                 <img src="img/team/team02.png" alt="" />
+                              </a>
+                              <div class="team-text">
+                                 <h5><a href="#">Jenifer</a></h5>
+                                 <p>Sales Manager</p>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-ms-6 col-xs-12 mb-sm-30">
+                           <div class="team-box text-center">
+                              <a class="team-img" href="#">
+                                 <img src="img/team/team03.png" alt="" />
+                              </a>
+                              <div class="team-text">
+                                 <h5><a href="#">Kathy Doe</a></h5>
+                                 <p>Fashion Designer</p>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-ms-6 col-xs-12 mb-sm-30">
+                           <div class="team-box text-center">
+                              <a class="team-img" href="#">
+                                 <img src="img/team/team04.png" alt="" />
+                              </a>
+                              <div class="team-text">
+                                 <h5><a href="#">Mrs. Doe</a></h5>
+                                 <p>Marketing Director</p>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="theme-feature section-padding">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="section-heading text-center">
+                              <h2>Theme Features</h2>
+                              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                           </div>                           
+                        </div>
+                        <div class="col-md-4">
+                           <div class="feature-box">
+                              <div class="feature-icon">
+                                 <i class="fa fa-lightbulb-o" aria-hidden="true"></i>
+                                 <h4>Creative Ideas</h4>
+                              </div>
+                              <p>Lorem ipsum dolor sit amet, eum ea meis simul definiebas, et ius modo latine praesent. Soluta sapientem sit ut. </p>
+                           </div>
+                        </div>
+                        <div class="col-md-4">
+                           <div class="feature-box">
+                              <div class="feature-icon">
+                                 <i class="fa fa-hashtag" aria-hidden="true"></i>
+                                 <h4>Beautiful Blog</h4>
+                              </div>
+                              <p>Lorem ipsum dolor sit amet, eum ea meis simul definiebas, et ius modo latine praesent. Soluta sapientem sit ut. </p>
+                           </div>
+                        </div>
+                        <div class="col-md-4">
+                           <div class="feature-box">
+                              <div class="feature-icon">
+                                 <i class="fa fa-heart-o" aria-hidden="true"></i>
+                                 <h4>SEO Friendly</h4>
+                              </div>
+                              <p>Lorem ipsum dolor sit amet, eum ea meis simul definiebas, et ius modo latine praesent. Soluta sapientem sit ut. </p>
+                           </div>
+                        </div>
+                        <div class="col-md-4">
+                           <div class="feature-box">
+                              <div class="feature-icon">
+                                 <i class="fa fa-bars" aria-hidden="true"></i>
+                                 <h4>Parallax Sections</h4>
+                              </div>
+                              <p>Lorem ipsum dolor sit amet, eum ea meis simul definiebas, et ius modo latine praesent. Soluta sapientem sit ut. </p>
+                           </div>
+                        </div>
+                        <div class="col-md-4">
+                           <div class="feature-box">
+                              <div class="feature-icon">
+                                 <i class="fa fa-cogs" aria-hidden="true"></i>
+                                 <h4>Powerfull Options</h4>
+                              </div>
+                              <p>Lorem ipsum dolor sit amet, eum ea meis simul definiebas, et ius modo latine praesent. Soluta sapientem sit ut. </p>
+                           </div>
+                        </div>
+                        <div class="col-md-4">
+                           <div class="feature-box">
+                              <div class="feature-icon">
+                                 <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+                                 <h4>Latest Design</h4>
+                              </div>
+                              <p>Lorem ipsum dolor sit amet, eum ea meis simul definiebas, et ius modo latine praesent. Soluta sapientem sit ut. </p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </section>
+            <!-- Content page wrapper -->
+         </div>
+         <!-- Page Wraper -->
+        <?php include("footer.php") ?>
+      </div>
+      <!-- Wraper -->
+      <!-- start jquery -->
+      <script src="js/jquery.min.js"></script>
+      <!-- jquery library js -->
+      <script src="js/jquery-ui.js"></script>
+      <!-- jquery ui js -->
+      <script src="js/tether.min.js"></script>
+      <!-- Bootstrap tooltips require Tether (Tether Js) -->
+      <script src="js/bootstrap.min.js"></script>
+      <!-- bootstrap js -->
+      <script src="js/owl.carousel.min.js"></script>
+      <!-- owl carousel js -->      
+      <script src="js/jquery.appear.js"></script>
+      <!-- appear js -->
+      <script src="js/jquery.countTo.js"></script>
+      <!-- countTo js -->
+      <script src="js/jquery.cookie.js"></script>
+      <!-- cookie js --> 
+      <script src="js/custom.js"></script>
+      <!-- custom js -->
+      <!-- end jquery -->
+   </body>
+
+<!-- Mirrored from magicclubgroup.com/bapor/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 01 Jan 2020 19:54:00 GMT -->
+</html>
+
